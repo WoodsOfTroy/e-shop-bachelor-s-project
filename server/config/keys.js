8de968b5820c8aa4e99436a2508887a734aaa327
Ruslan <58141049+WoodsOfTroy@users.noreply.github.com>
2022-05-30 10:30:56 +0300

    Delete server directory

--- server/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://root_admin:WRMiAyYg8OUWBC3P@cluster0.f7ie4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- server/routes/payment.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/payment.controller');
-const router = express.Router();
-
-router.get('/', controller.getPayment);
-
-module.exports = router;
